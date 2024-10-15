e5d4f6c49ea78c5b312a9b7db1db8814990db11c
Tyler <tyler.audino@ufl.edu>
2024-10-15 15:21:58 -0400

    created separate pages and linked them together

---
+++ lib/goals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoalsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Goals Page'),
+      ),
+      body: Center(
+        child: Text('This is the Goals page'),
+      ),
+    );
+  }
+}
