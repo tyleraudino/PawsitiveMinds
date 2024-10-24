497b43a9c69f3d02e95105b265f2925b1e3b9bae
PythiaUF <125420118+PythiaUF@users.noreply.github.com>
2024-10-23 21:50:21 -0400

    feat: add base for login/registering

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
 from typing import Optional
+import datetime
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from bson import ObjectId
 import motor.motor_asyncio
+import argon2
+import jwt
 
+ph = argon2.PasswordHasher()
 app = FastAPI()
+
+# todo - secret key should be stored in environment variable
+SECRET_KEY = "76b33515ec3f0b4a1f30cb3f78e0aaa880662251c07144fc69b18442974ff221"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 1 week
 
 # CORS middleware
 app.add_middleware(
@@ -35,10 +44,54 @@ class Goal(BaseModel):
     title: str
     description: str
 
+# for user data
+class User(BaseModel):
+    user_id: str
+    password: str
+
 # for updating goals
 class UpdateGoal(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
+
+    
+def generate_jwt(user_id: str) -> str:
+    to_encode = {
+        "user_id": user_id,
+        "expires_in": (datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)).isoformat()
+    }
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# registers user
+@app.post("/user/register")
+async def register_user(user: User):
+    if await collection.find_one({"user_id": user.user_id}):
+        raise HTTPException(status_code=400, detail="User already registered")
+
+    user_data = user.model_dump()
+    user_data["password"] = ph.hash(user_data["password"])
+    result = await collection.insert_one(user_data)
+    if result.inserted_id:
+        return {"message": "User registered successfully!", "token": generate_jwt(user.user_id)}
+    else:
+        raise HTTPException(status_code=400, detail="User registration failed")
+
+# login user
+@app.post("/user/login")
+async def login_user(user: User):
+    user_data = await collection.find_one({"user_id": user.user_id})
+    if not user_data:
+        raise HTTPException(status_code=400, detail="User not found")
+    
+    if not ph.verify(user_data["password"], user.password):
+        raise HTTPException(status_code=400, detail="Invalid password")
+    
+    if ph.check_needs_rehash(user_data["password"]):
+        user_data["password"] = ph.hash(user.password)
+        await collection.update_one({"_id": user_data["_id"]}, {"$set": {"password": user_data["password"]}})
+    
+    return {"message": "Login successful!", "token": generate_jwt(user.user_id)}
 
 # get request to fetch all goals
 @app.get("/goals/")
