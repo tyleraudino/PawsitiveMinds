0c900ff3eee43736dcf7989ea10edbafb7dcba77
Tyler <tyler.audino@ufl.edu>
2024-10-27 13:53:40 -0400

    add points and base completion page

--- lib/goals.dart
+++ lib/goals.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/services.dart';
+
 
 class Goal {
   String title;
   String description;
+  // String recurrence = "Daily"; 
+  // DateTime endDate = DateTime(10000, 1, 1); // init end date to year 10000 for reminders with no end date
+  // bool reminders = false;
+  int points;
 
-  Goal({required this.title, required this.description});
+  Goal({required this.title, required this.description, required this.points});
 }
 
 Future<void> createGoal(Goal goal) async {
@@ -43,8 +49,8 @@ class GoalsPage extends StatefulWidget {
 class _GoalsPageState extends State<GoalsPage>  {
   List<Goal> userGoals = [
     //example data - will get from backend but implement later 
-    Goal(title: 'Meditate', description: 'Meditate 10 minutes every morning'),
-    Goal(title: 'Exercise', description: 'Exercise 5 times a week'),
+    Goal(title: 'Meditate', description: 'Meditate 10 minutes every morning', points: 10),
+    Goal(title: 'Exercise', description: 'Exercise 5 times a week', points: 20),
   ];
 
   @override
@@ -59,7 +65,16 @@ class _GoalsPageState extends State<GoalsPage>  {
           final goal = userGoals[index];
           return Card(
             child: ListTile(
-              leading: Icon(Icons.check_box_rounded),
+              leading: IconButton(
+                icon: Icon(Icons.check_circle),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CompleteGoalPage(goal: goal),
+                    ),
+                  );
+                } ,),
               title: Text(goal.title),
               subtitle: Text(goal.description),
               trailing: IconButton(
@@ -123,27 +138,33 @@ class EditGoalPage extends StatefulWidget {
 class _EditGoalPageState extends State<EditGoalPage> {
   late TextEditingController titleController;
   late TextEditingController descriptionController;
+  late TextEditingController pointsController;
 
   @override
   void initState() {
+
     super.initState();
     titleController = TextEditingController(text: widget.goal?.title ?? '');
     descriptionController = TextEditingController(text: widget.goal?.description ?? '');
+    pointsController = TextEditingController(text: widget.goal != null ? widget.goal!.points.toString() : '0',);
   }
 
   @override
   void dispose() {
     titleController.dispose();
     descriptionController.dispose();
+    pointsController.dispose();
     super.dispose();
   }
 
   void saveGoal() {
+    int points = int.tryParse(pointsController.text) ?? 0;
     Navigator.pop(
       context,
       Goal(
         title: titleController.text,
         description: descriptionController.text,
+        points: points,
       ),
     );
   }
@@ -164,10 +185,67 @@ class _EditGoalPageState extends State<EditGoalPage> {
               controller: descriptionController,
               decoration: InputDecoration(labelText: 'Description'),
             ),
+            TextField(
+              controller: pointsController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(labelText: 'Points'),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: saveGoal,
               child: Text(widget.isEditing ? 'Save' : 'Create'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CompleteGoalPage extends StatefulWidget{
+  final Goal goal;
+
+  CompleteGoalPage({required this.goal});
+
+  @override
+  _CompleteGoalPageState createState() => _CompleteGoalPageState();
+}
+
+class _CompleteGoalPageState extends State<CompleteGoalPage> {
+  late TextEditingController journalController;
+
+  @override
+  void initState() {
+    super.initState();
+    journalController = TextEditingController(text: '');
+  }
+
+  @override
+  void dispose() {
+    journalController.dispose();
+    super.dispose();
+  }
+
+  void saveCompletion() {
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: journalController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: saveCompletion,
+              child: Text("Complete Goal"),
             ),
           ],
         ),
